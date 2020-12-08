c5dc91466e60e591ba2f83c0322bda5e0e206730
A. Kaan Atesoglu <aka43@case.edu>
2020-12-07 21:44:41 -0500

    fleshed out readme, upgraded various program peices

--- Program.cs
+++ Program.cs
@@ -31,21 +31,24 @@ namespace readthis_doc_maker
                 }
             } else {
                 // default behavior
-                modePairs[RunOptions.Silent] = true;
+                modePairs[RunOptions.Interactive] = true;
                 modePairs[RunOptions.Opinionated] = true;
             }
 
             // initilize
-            StreamWriter streamOut = new StreamWriter("README.txt");
+            StreamWriter streamOut = new StreamWriter("READTHIS.MD");
             DirectoryInfo rootInfo = new DirectoryInfo(".");
             DirectoryInfo[] dirInfos = rootInfo.GetDirectories();
             FileInfo[] fileInfos = rootInfo.GetFiles();
 
-
             // write header information
-            streamOut.WriteLine(FormatHeader(dirInfos.Length, fileInfos.Length));
-            streamOut.WriteLine(DoBreak());
-
+            if (modePairs[RunOptions.Interactive]) {
+                streamOut.WriteLine(FormatHeader(DoPrompt("Describe what the current folder is for..."), dirInfos.Length, fileInfos.Length));
+            } else {
+                streamOut.WriteLine(FormatHeader(string.Empty, dirInfos.Length, fileInfos.Length));
+            }
+            streamOut.WriteLine(Break());
+            
             // loop through directories
             foreach (DirectoryInfo dir in dirInfos) {
                 streamOut.WriteLine("./" + dir.Name);
@@ -59,19 +62,38 @@ namespace readthis_doc_maker
                 streamOut.WriteLine();
             }
 
+            // clean up
+            Console.Clear();
             streamOut.Close();
             
         }
 
-        static string DoBreak() {
-            return "***********************";
+        static string DoPrompt(string prompt) {
+            
+                Console.Clear();
+                Console.WriteLine(prompt);
+                return Console.ReadLine();
         }
 
-        static string FormatHeader(int dirCount, int fileCount) {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("By: {0}, created at: {1} \n", Environment.UserName, DateTime.Now);
-            sb.AppendFormat("{0} dirs | {1} fldrs | {2} all", dirCount, fileCount, dirCount + fileCount);
+        static string Break() {
+            return "---------------------------------------\n";
+        }
 
+        static string FormatHeader(string description, int dirCount, int fileCount) {
+            FileInfo currFile = new FileInfo(Environment.CurrentDirectory);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("# {0}\n", currFile.Name);
+            //sb.AppendFormat("## @ {1}\n", currFile);
+            if (description != string.Empty) {
+                sb.AppendFormat("_{0}_\n", description);
+            } else {
+                sb.Append("\n");
+            }
+            
+            sb.Append(Break());
+            sb.AppendFormat("By: {0}, created at: {1} \n", Environment.UserName, DateTime.Now);
+            sb.AppendFormat("{0} dirs | {1} fldrs | {2} all \n", dirCount, fileCount, dirCount + fileCount);
             return sb.ToString();
         }
 
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ReadThis
 Command Line program to spit out a descriptive README for the current folder and it's contents
+
+_Are you tired of trying to work in a folder, but it's full of poorly named items, abandoned files, system files, and other assorted accoutrement? Do you die on the inside when you see **another** folder called "temp"? Does it make you wanna "rm -rf" a fresh crater for future gernerations to learn from?_
+
+_This aims to help clean that up._
+
+---
+
+## What does it do?
+This utility iterates over the current folders and files and creates a README.txt that attempts to describe each folder and file.  It scans attributes and marks system folders, or folders found inside the environment.
+
+Currently:
+- Creates a formatted header about the Readme creator.
+- Lists out each folder/file
+- Identifies system folders/files
+
+Targets Windows/Linux/MacOS
+
+---
+
+## What will it not do?
+- It will not modify any files or folders
+
+-----
+
+# Arguments & Options
+|Arg | Mode        | Description
+|----|-------------|--------------
+| -s | Silent      | Do not prompt for user input
+| -o | Opinionated | (_default_) Mark more opinionated things like: is a file stale? does it have a bad name?
+| -i | Interactive | (_default_) Prompt for descriptions and more
+
+# TODO
+- [ ] Folders/Files: identify if they are mentioned in the system path.
+- [ ] Convert everything to nicely formatted markdown
+- [ ] Add more opinionated pieces
