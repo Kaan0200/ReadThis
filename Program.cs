99f3e83ee30b4f957f1e1c569790f41b7efebeb0
A. Kaan Atesoglu <aka43@case.edu>
2020-12-07 20:24:55 -0500

    added program so far

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace readthis_doc_maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // identify run options
+            Dictionary<RunOptions, bool> modePairs = new Dictionary<RunOptions, bool>();
+            modePairs.Add(RunOptions.Silent, false);
+            modePairs.Add(RunOptions.Interactive, false);
+            modePairs.Add(RunOptions.Opinionated, false);
+
+            if (args.Length != 0) {
+                foreach (string arg in args) {
+                    switch (arg) {
+                        case ("-s"): 
+                            modePairs[RunOptions.Silent] = true;
+                        break;
+                        case ("-i"):
+                            modePairs[RunOptions.Interactive] = true;
+                        break;
+                        case ("-o"):
+                            modePairs[RunOptions.Opinionated] = true;
+                        break;
+                    }
+                }
+            } else {
+                // default behavior
+                modePairs[RunOptions.Silent] = true;
+                modePairs[RunOptions.Opinionated] = true;
+            }
+
+            // initilize
+            StreamWriter streamOut = new StreamWriter("README.txt");
+            DirectoryInfo rootInfo = new DirectoryInfo(".");
+            DirectoryInfo[] dirInfos = rootInfo.GetDirectories();
+            FileInfo[] fileInfos = rootInfo.GetFiles();
+
+
+            // write header information
+            streamOut.WriteLine(FormatHeader(dirInfos.Length, fileInfos.Length));
+            streamOut.WriteLine(DoBreak());
+
+            // loop through directories
+            foreach (DirectoryInfo dir in dirInfos) {
+                streamOut.WriteLine("./" + dir.Name);
+                streamOut.WriteLine(FormatAttributes(dir.Attributes));
+                streamOut.WriteLine();
+            }
+            // loop through files
+            foreach (FileInfo file in fileInfos) {
+                streamOut.WriteLine("  " + file.Name);
+                streamOut.WriteLine(FormatAttributes(file.Attributes));
+                streamOut.WriteLine();
+            }
+
+            streamOut.Close();
+            
+        }
+
+        static string DoBreak() {
+            return "***********************";
+        }
+
+        static string FormatHeader(int dirCount, int fileCount) {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("By: {0}, created at: {1} \n", Environment.UserName, DateTime.Now);
+            sb.AppendFormat("{0} dirs | {1} fldrs | {2} all", dirCount, fileCount, dirCount + fileCount);
+
+            return sb.ToString();
+        }
+
+        static string FormatAttributes(FileAttributes attributes) {
+            if((attributes & FileAttributes.System) == FileAttributes.System) {
+                return "> [SYSTEM]";
+            } else {
+                return "";
+            }
+            
+        }
+    }
+
+    enum RunOptions {
+        Interactive, // ask a question for each file
+        Opinionated, // write suggestive information, like if file hasn't been touched in years
+        Silent, // no prompts
+    }
+}
